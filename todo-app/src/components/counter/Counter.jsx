d8cf3b05332d1a5951ff05500e961bff5372db68
jeisonrengifo <jeisonrengifo@gmail.com>
2020-08-21 17:07:18 -0400

    Counter calling method from the child

--- todo-app/src/components/counter/Counter.jsx
+++ todo-app/src/components/counter/Counter.jsx
@@ -3,16 +3,51 @@ import React, {Component} from 'react'
 import './counter.css'
 import CounterButton from './CounterButton'
 class Counter extends Component{
+    constructor(){
+        super()
+        this.state={
+            counter:0
+        }
+        this.increment=this.increment.bind(this);
+        this.decrement=this.decrement.bind(this);
+        this.reset=this.reset.bind(this);
+    }
     render (){
         return(
             <div className="counter">
-                <CounterButton by={1}></CounterButton>
-                <CounterButton by={5}></CounterButton>
-                <CounterButton by={10}></CounterButton>
+                <CounterButton by={1} incrementMethod={this.increment} decrementMethod={this.decrement}></CounterButton>
+                <CounterButton by={5} incrementMethod={this.increment} decrementMethod={this.decrement}></CounterButton>
+                <CounterButton by={10} incrementMethod={this.increment} decrementMethod={this.decrement}></CounterButton>
+                <span className="count">{this.state.counter}</span>
+                <div>
+                    <button className="resetButton" onClick={this.reset}>Reset</button>
+                </div> 
+                
+                
+
             </div>
 
         )
     }
+    increment(by){
+        this.setState({
+            counter:this.state.counter+by
+        })
+    }
+
+    decrement(by){
+        this.setState({
+            counter:this.state.counter-by
+        })
+    }
+
+    reset(){
+        this.setState({
+            counter:0
+        })
+    }
+
+  
     
 }
 export default Counter
--- todo-app/src/components/counter/CounterButton.jsx
+++ todo-app/src/components/counter/CounterButton.jsx
@@ -7,22 +7,29 @@ class CounterButton extends Component{
             counter:0
         }
         this.increment=this.increment.bind(this);
+        this.decrement=this.decrement.bind(this);
     }
     render (){
         return(
             <div className="counter">
                 <button onClick={this.increment}>+{this.props.by}</button>
-                <span className="count">{this.state.counter}</span>
+                <button onClick={this.decrement}>-{this.props.by}</button>
+               {/* <span className="count">{this.state.counter}</span>*/}
             </div>
 
         )
     }
 
     increment(){
-        this.setState({
+        {/*this.setState({
             counter:this.state.counter+this.props.by
-        });    
+        });*/}    
+        this.props.incrementMethod(this.props.by);
 
+    }
+
+    decrement(){
+        this.props.decrementMethod(this.props.by);
     }
 }
 export default CounterButton
