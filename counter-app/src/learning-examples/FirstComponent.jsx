be68491187eacd3aa8cc0408fe3e7d8568168285
jeisonrengifo <jeisonrengifo@gmail.com>
2020-08-23 22:59:29 -0400

    change name todo-app for counter-app

---
+++ counter-app/src/learning-examples/FirstComponent.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class FirstComponent extends Component{
+    render(){
+        return(
+            <div className="firstComponente">
+                This is my first component as a module!
+            </div>
+        );
+    }
+}
+export default FirstComponent
---
+++ counter-app/src/learning-examples/OtherComponent.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+ class OtherComponent extends Component{
+    render(){
+    return(
+        <div className="function1">
+            This module is a class
+        </div>
+    );
+}
+
+}
+export default OtherComponent
